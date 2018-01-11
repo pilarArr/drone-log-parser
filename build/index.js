e48e0a070885d11136a13c08f174e8127efe55dd
Pilar Arregui <parregui@srmconsulting.es>
2018-01-11 16:24:08 +0100

    Update build

--- build/index.js
+++ build/index.js
@@ -3,19 +3,19 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.txtObject2JSON = exports.logObject2JSON = exports.txt2JSON = exports.log2JSON = exports.log2var = exports.txt2var = undefined;
+exports.log2var = exports.txt2var = exports.log2JSON = exports.txt2JSON = exports.logObject2JSON = exports.txtObject2JSON = undefined;
 
 var _lineReader = require('line-reader');
 
-var lineReader = _interopRequireWildcard(_lineReader);
+var _lineReader2 = _interopRequireDefault(_lineReader);
 
 var _fs = require('fs');
 
-var fs = _interopRequireWildcard(_fs);
+var _fs2 = _interopRequireDefault(_fs);
 
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var txt2var = exports.txt2var = function txt2var(myLogPath, callback) {
+var txt2var = function txt2var(myLogPath, callback) {
   var myLogVar = {};
 
   function parseDataLog(dataLine) {
@@ -58,11 +58,11 @@ var txt2var = exports.txt2var = function txt2var(myLogPath, callback) {
     myLogVar[dataName][lastIndexInDataArray] = dataLogObject;
   }
 
-  if (!fs.existsSync(myLogPath)) {
+  if (!_fs2.default.existsSync(myLogPath)) {
     throw new Error('File does not exist');
   }
 
-  lineReader.eachLine(myLogPath, function (line, last) {
+  _lineReader2.default.eachLine(myLogPath, function (line, last) {
     // eslint-disable-line
     parseLine(line);
     if (last) {
@@ -71,7 +71,7 @@ var txt2var = exports.txt2var = function txt2var(myLogPath, callback) {
   });
 };
 
-var log2var = exports.log2var = function log2var(myLogPath, callback) {
+var log2var = function log2var(myLogPath, callback) {
   var myLogVar = {};
 
   function parseFMT(lineArray) {
@@ -131,7 +131,7 @@ var log2var = exports.log2var = function log2var(myLogPath, callback) {
     }
   }
 
-  lineReader.eachLine(myLogPath, function (line) {
+  _lineReader2.default.eachLine(myLogPath, function (line) {
     // eslint-disable-line
     var lineArr = line.split(', ');
 
@@ -143,7 +143,7 @@ var log2var = exports.log2var = function log2var(myLogPath, callback) {
     }
   });
 
-  lineReader.eachLine(myLogPath, function (line, last) {
+  _lineReader2.default.eachLine(myLogPath, function (line, last) {
     var lineArr = line.split(', ');
 
     if (lineArr[0] !== 'FMT') {
@@ -156,7 +156,7 @@ var log2var = exports.log2var = function log2var(myLogPath, callback) {
   });
 };
 
-var log2JSON = exports.log2JSON = function log2JSON(myLogPath) {
+var log2JSON = function log2JSON(myLogPath) {
   var myJSONPath = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : myLogPath + '.json';
 
   log2var(myLogPath, function (myLogVar) {
@@ -172,15 +172,15 @@ var log2JSON = exports.log2JSON = function log2JSON(myLogPath) {
 
     // add numbers if path exists. do not overwrite or delete
     var copy = 1;
-    while (fs.existsSync(myFinalPath)) {
+    while (_fs2.default.existsSync(myFinalPath)) {
       myFinalPath = myFinalPath.slice(0, -5) + copy + myFinalPath.slice(-5);
       copy += 1;
     }
-    fs.appendFileSync(myFinalPath, JSON.stringify(myLogVar));
+    _fs2.default.appendFileSync(myFinalPath, JSON.stringify(myLogVar));
   });
 };
 
-var txt2JSON = exports.txt2JSON = function txt2JSON(myLogPath) {
+var txt2JSON = function txt2JSON(myLogPath) {
   var myJSONPath = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : myLogPath + '.json';
 
   txt2var(myLogPath, function (myLogVar) {
@@ -196,72 +196,155 @@ var txt2JSON = exports.txt2JSON = function txt2JSON(myLogPath) {
 
     // add numbers if path exists. do not overwrite or delete
     var copy = 1;
-    while (fs.existsSync(myFinalPath)) {
+    while (_fs2.default.existsSync(myFinalPath)) {
       myFinalPath = myFinalPath.slice(0, -5) + copy + myFinalPath.slice(-5);
       copy += 1;
     }
-    fs.appendFileSync(myFinalPath, JSON.stringify(myLogVar));
+    _fs2.default.appendFileSync(myFinalPath, JSON.stringify(myLogVar));
   });
 };
 
-var logObject2JSON = exports.logObject2JSON = function logObject2JSON(object2Extract, myLogPath) {
-  var myJSONPath = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : myLogPath + '-' + object2Extract + '.json';
+var logObject2JSON = function logObject2JSON(object2Extract, myLogPath, myJSONPath) {
+  var myLogVar = {};
+  myLogVar[object2Extract] = [];
 
-  log2var(myLogPath, function (myLogVar) {
-    // check for json extension
-    var myFinalPath = myJSONPath;
-    var myExtension = myFinalPath.slice(-5);
-    if (myExtension !== '.json') {
-      myFinalPath = myJSONPath + '.json';
-    }
-    if (myFinalPath.length === 5) {
-      myFinalPath = myLogPath + '-' + object2Extract + '.json';
-    }
+  var logMetaData = {};
+  logMetaData.FMT = [];
 
-    // add numbers if path exists. do not overwrite or delete
-    var copy = 1;
-    while (fs.existsSync(myFinalPath)) {
-      myFinalPath = myFinalPath.slice(0, -5) + copy + myFinalPath.slice(-5);
-      copy += 1;
+  if (!_fs2.default.existsSync(myLogPath)) {
+    throw new Error('Log File Does not Exist');
+  }
+
+  var myColumnMetadata = [];
+
+  function findMyMetadata() {
+    // Search for the FMT value to get columns
+    // TODO try to make this bisection search after a merge-sort of the FMT
+    // array by name to make the search faster.
+    var numberOfFMTMessages = logMetaData.FMT.length;
+    for (var i = 0; i < numberOfFMTMessages; i += 1) {
+      if (logMetaData.FMT[i].Name === object2Extract) {
+        myColumnMetadata = logMetaData.FMT[i].Columns;
+        break;
+      }
     }
-    var logHasObject = Object.prototype.hasOwnProperty.call(myLogVar, object2Extract);
-    if (logHasObject) {
-      var result = {};
-      result[object2Extract] = myLogVar[object2Extract];
-      fs.appendFileSync(myFinalPath, JSON.stringify(result));
-    } else {
-      throw new Error(object2Extract + ' not in log');
+  }
+
+  function parseFMT(lineArray) {
+    var columns = ['Type', 'Length', 'Name', 'Format', 'Columns'];
+    var lastIndexInFTM = logMetaData.FMT.length;
+    for (var i = 1; i < lineArray.length; i += 1) {
+      if (i < 5) {
+        logMetaData.FMT[lastIndexInFTM][columns[i - 1]] = lineArray[i];
+      } else if (i === 5) {
+        logMetaData.FMT[lastIndexInFTM][columns[i - 1]] = [];
+        var mycols = lineArray[i].split(',');
+        for (var j = 0; j < mycols.length; j += 1) {
+          logMetaData.FMT[lastIndexInFTM][columns[i - 1]].push(mycols[j]);
+        }
+      }
+    }
+  }
+
+  function parseMSG(lineArray) {
+    // Check if object exists and create one if it doesnt
+    // Parse the column
+    var lastIndexInDataArray = myLogVar[object2Extract].length;
+    for (var i = 1; i < lineArray.length; i += 1) {
+      var columnName = myColumnMetadata[i - 1];
+      if (Number.isNaN(Number(lineArray[i]))) {
+        myLogVar[object2Extract][lastIndexInDataArray][columnName] = lineArray[i];
+      } else {
+        myLogVar[object2Extract][lastIndexInDataArray][columnName] = Number(lineArray[i]);
+      }
+    }
+  }
+
+  _lineReader2.default.eachLine(myLogPath, function (line, last) {
+    // eslint-disable-line
+    var lineArr = line.split(', ');
+
+    if (lineArr[0] === 'FMT') {
+      parseFMT(lineArr);
+    }if (last) {
+      findMyMetadata();
+    }
+  });
+
+  _lineReader2.default.eachLine(myLogPath, function (line, last) {
+    var lineArr = line.split(', ');
+
+    if (lineArr[0] === object2Extract) {
+      parseMSG(lineArr);
+    }
+    if (last) {
+      if (_fs2.default.existsSync(myJSONPath)) {
+        _fs2.default.unlinkSync(myJSONPath);
+      }
+      _fs2.default.appendFileSync(myJSONPath, JSON.stringify(myLogVar));
     }
   });
 };
 
-var txtObject2JSON = exports.txtObject2JSON = function txtObject2JSON(object2Extract, myLogPath) {
-  var myJSONPath = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : myLogPath + '-' + object2Extract + '.json';
+var txtObject2JSON = function txtObject2JSON(object2Extract, myLogPath, myJSONPath) {
+  var myLogVar = {};
+  myLogVar[object2Extract] = [];
 
-  txt2var(myLogPath, function (myLogVar) {
-    // check for json extension
-    var myFinalPath = myJSONPath;
-    var myExtension = myFinalPath.slice(-5);
-    if (myExtension !== '.json') {
-      myFinalPath = myJSONPath + '.json';
-    }
-    if (myFinalPath.length === 5) {
-      myFinalPath = myLogPath + '-' + object2Extract + '.json';
-    }
+  if (!_fs2.default.existsSync(myLogPath)) {
+    throw new Error('Log File Does not Exist');
+  }
 
-    // add numbers if path exists. do not overwrite or delete
-    var copy = 1;
-    while (fs.existsSync(myFinalPath)) {
-      myFinalPath = myFinalPath.slice(0, -5) + copy + myFinalPath.slice(-5);
-      copy += 1;
+  function parseDataLog(dataLine) {
+    var removeBrakets = dataLine.substr(1).slice(0, -1);
+    var splitItems = removeBrakets.split(', ');
+
+    var resultObject = {};
+    splitItems.forEach(function (keyValue) {
+      var splitKeyValue = keyValue.split(' : ');
+      var itemKey = splitKeyValue[0];
+      var itemData = splitKeyValue[1];
+      if (Number.isNaN(Number(itemData))) {
+        resultObject[itemKey] = itemData;
+      } else {
+        resultObject[itemKey] = Number(itemData);
+      }
+    });
+    return resultObject;
+  }
+
+  function parseLine(textLine) {
+    var lineArr = textLine.split(' ');
+    var finalLineArr = lineArr.slice(0, 3);
+    finalLineArr.push(lineArr.slice(3).join(' '));
+    finalLineArr[1] = finalLineArr[1].slice(0, -1);
+
+    var timeStamp = finalLineArr.slice(0, 2).join(' ');
+    var dataName = finalLineArr[2];
+    var dataLog = finalLineArr[3];
+
+    if (dataName === object2Extract) {
+      var lastIndexInDataArray = myLogVar[object2Extract].length;
+
+      var dataLogObject = parseDataLog(dataLog);
+      dataLogObject.TimeLog = timeStamp;
+      myLogVar[dataName][lastIndexInDataArray] = dataLogObject;
     }
-    var logHasObject = Object.prototype.hasOwnProperty.call(myLogVar, object2Extract);
-    if (logHasObject) {
-      var result = {};
-      result[object2Extract] = myLogVar[object2Extract];
-      fs.appendFileSync(myFinalPath, JSON.stringify(result));
-    } else {
-      throw new Error(object2Extract + ' not in log');
+  }
+
+  _lineReader2.default.eachLine(myLogPath, function (line, last) {
+    parseLine(line);
+    if (last) {
+      if (_fs2.default.existsSync(myJSONPath)) {
+        _fs2.default.unlinkSync(myJSONPath);
+      }
+      _fs2.default.appendFileSync(myJSONPath, JSON.stringify(myLogVar));
     }
   });
 };
+
+exports.txtObject2JSON = txtObject2JSON;
+exports.logObject2JSON = logObject2JSON;
+exports.txt2JSON = txt2JSON;
+exports.log2JSON = log2JSON;
+exports.txt2var = txt2var;
+exports.log2var = log2var;
